07d8ccefc30c2e8c2c3202b18d95b4846b8aa824
Giorgi-Bahuta <giobahuta@gmail.com>
2023-12-05 14:37:46 +0300

    Добавил новый js файл и прикрепил его к html

--- hw-5.js
+++ hw-5.js
@@ -71,12 +71,4 @@ console.log(circle1.getPerimeter());
 console.log(circle2.getArea());
 console.log(circle2.getPerimeter());
 
-//Задание 8
-function numMonth() {
-    let userNum = Number(prompt('Введит число, а я скажу к какому времени года он относится'));
-    (userNum === 3) || (userNum === 4) || (userNum === 5) ? alert('Весна') :
-    (userNum === 6) || (userNum === 7) || (userNum === 8) ? alert('Лето') :
-    (userNum === 9) || (userNum === 10) || (userNum === 11) ? alert('Осень') :
-    (userNum === 12) || (userNum === 1) || (userNum === 2) ? alert('Зима') :
-    alert('Некорректное число')
-}
+//Задание 8 Разместил в файле script.js
